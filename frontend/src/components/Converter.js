018020a61e5d5a5d2d0500710523cb452ba81fbf
domasgrigaliunas <domasjkm@gmail.com>
2020-09-22 10:26:52 +0300

    converter.js update

--- frontend/src/components/Converter.js
+++ frontend/src/components/Converter.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
 import { getCurrentDate } from "../utils";
-
 import "./Converter.css";
 
 class Converter extends Component {
@@ -11,55 +10,52 @@ class Converter extends Component {
     toCurrency: "USD",
     amount: 1,
     currencies: [],
-    currencyLabels: [],
     conversionData: [],
   };
 
-  // Initializes the currencies with values from the api
+  // Saves user activity to DB
+  sendUserActivity() {
+    axios({
+      method: "post",
+      url: "http://localhost:8080/api/v1/UserActivity",
+      headers: {},
+      data: {
+        dateTime: String(getCurrentDate()),
+        amount: String(this.state.amount),
+        currencyA: String(this.state.fromCurrency),
+        currencyB: String(this.state.toCurrency),
+        result: String(this.state.result),
+      },
+    });
+  }
+
+  // Initializes names of currencies to dropdowns
   componentDidMount() {
     axios
-      //.get("http://api.openrates.io/latest")
       .get("http://localhost:8080/api/v1/currencyLabels")
       .then((response) => {
-        // Initialized with 'EUR' because the base currency is 'EUR'
-        // and it is not included in the response
         const currencyAr = [];
-        const currencyLabelsAr = [];
-
         response.data.forEach(function (entry) {
-          console.log(entry);
-          currencyLabelsAr.push(entry);
           currencyAr.push(entry.currency);
         });
-        /*
-        for (const key in response.data) {
-          currencyAr.push(key);
-        }
-        */
-
-        console.log(response.data);
 
         this.setState({
-          currencies: currencyAr.sort(),
-          currencyLabels: currencyLabelsAr,
+          currencies: currencyAr,
         });
       })
       .catch((err) => {
-        console.log("Opps", err.message);
+        console.log("Error: ", err.message);
       });
   }
 
   // Event handler for the conversion
   convertHandler = () => {
-    console.log(this.state.fromCurrency + " " + this.state.toCurrency);
-
     if (this.state.fromCurrency !== this.state.toCurrency) {
       if (
         !(this.state.fromCurrency !== "EUR" && this.state.toCurrency !== "EUR")
       ) {
         axios
           .get(
-            //`http://api.openrates.io/latest?base=${this.state.fromCurrency}&symbols=${this.state.toCurrency}`
             `http://localhost:8080/api/v1/User?currency=${
               this.state.fromCurrency === "EUR"
                 ? this.state.toCurrency
@@ -67,87 +63,38 @@ class Converter extends Component {
             }`
           )
           .then((response) => {
-            if (this.state.fromCurrency === "EUR") {
-              const result =
-                (this.state.amount * response.data.amountB).toFixed(5) +
-                " " +
-                response.data.currency_b;
-              //result = result.toFixed(5) + " " + response.data.currencyB;
-              console.log(result);
-              this.setState({ result: result });
+            if (response.data.last_updated !== "NA") {
+              if (this.state.fromCurrency === "EUR") {
+                const result =
+                  (this.state.amount * response.data.amountB).toFixed(5) +
+                  " " +
+                  response.data.currency_b;
+                this.setState({ result: result });
+              } else {
+                const result =
+                  (this.state.amount / response.data.amountB).toFixed(5) +
+                  " " +
+                  response.data.currency_b;
+                this.setState({ result: result });
+              }
             } else {
-              const result =
-                (this.state.amount / response.data.amountB).toFixed(5) +
-                " " +
-                response.data.currency_b;
-              this.setState({ result: result });
+              this.setState({ result: "There is no data for this conversion" });
             }
-            console.log(
-              getCurrentDate() +
-                " " +
-                this.state.amount +
-                " " +
-                this.state.fromCurrency +
-                " " +
-                this.state.toCurrency +
-                " " +
-                this.state.result
-            );
 
-            axios({
-              method: "post",
-              url: "http://localhost:8080/api/v1/UserActivity",
-              headers: {},
-              data: {
-                dateTime: String(getCurrentDate()),
-                amount: String(this.state.amount),
-                currencyA: String(this.state.fromCurrency),
-                currencyB: String(this.state.toCurrency),
-                result: String(this.state.result),
-              },
-            });
-
-            //   const result =
-            //     this.state.amount * response.data.rates[this.state.toCurrency];
-            //   this.setState({ result: result.toFixed(5) });
+            this.sendUserActivity();
           })
 
           .catch((err) => {
-            console.log("Opps", err.message);
+            console.log("Error: ", err.message);
           });
       } else {
         this.setState({ result: "Please select one currency as EUR" });
+        this.sendUserActivity();
       }
     } else {
-      this.setState({ result: "You cant convert the same currency!" });
+      this.setState({ result: "You cannot convert the same currency!" });
+      this.sendUserActivity();
     }
-
-    // var params = new URLSearchParams();
-    // params.append("dateTime", String(getCurrentDate()));
-    // params.append("amount", String(this.state.amount));
-    // params.append("currencyA", String(this.state.fromCurrency));
-    // params.append("currencyB", String(this.state.toCurrency));
-    // params.append("result", String(this.state.result));
-
-    // axios
-    //   .post("http://localhost:8080/api/v1/UserActivity", params)
-    //   .catch((err) => {
-    //     console.log("Opps", err.message);
-    //   });
-
-    // axios
-    //   .post("http://localhost:8080/api/v1/UserActivity", {
-    //     headers: {
-    //       dateTime: String(getCurrentDate()),
-    //       amount: String(this.state.amount),
-    //       currencyA: String(this.state.fromCurrency),
-    //       currencyB: String(this.state.toCurrency),
-    //       result: String(this.state.result),
-    //     },
-    //   })
-    //   .catch((err) => {
-    //     console.log("Opps", err.message);
-    //   });
   };
 
   // Updates the states based on the dropdown that was changed
@@ -158,11 +105,9 @@ class Converter extends Component {
     if (event.target.name === "to") {
       this.setState({ toCurrency: event.target.value });
     }
-    //console.log(this.state.fromCurrency + " " + this.state.toCurrency);
   };
 
   render() {
-    console.log(this.state.currencyLabels);
     return (
       <div className="Converter">
         <h2>
@@ -184,8 +129,8 @@ class Converter extends Component {
             onChange={(event) => this.selectHandler(event)}
             value={this.state.fromCurrency}
           >
-            {this.state.currencyLabels.map((cur) => (
-              <option key={cur.currency}>{cur.currency}</option>
+            {this.state.currencies.map((cur) => (
+              <option key={cur}>{cur}</option>
             ))}
           </select>
           To
@@ -201,7 +146,7 @@ class Converter extends Component {
           <button onClick={this.convertHandler}>Convert</button>
         </div>
         {this.state.result && <h3>{this.state.result}</h3>}
-        <div></div>
+        <div className="extraData"></div>
       </div>
     );
   }
